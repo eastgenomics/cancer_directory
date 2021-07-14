13bf23e59328390116121df7b8f41cb1ba1fabff
Jay-Miles <jay.miles@addenbrookes.nhs.uk>
2021-07-14 12:48:34 +0100

    added config file in gitignore

--- ngtd_cancer/settings.py
+++ ngtd_cancer/settings.py
@@ -19,6 +19,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
+from config import SECRET_KEY
+SECRET_KEY = SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
