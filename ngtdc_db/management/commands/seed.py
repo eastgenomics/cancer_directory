88da524f73e9b9bc1016ba4bd4e6b235bc9ba5fc
Jay-Miles <jay.miles@addenbrookes.nhs.uk>
2021-07-22 09:33:01 +0100

    changed seed.py command to take directory version argument rather than filename

--- ngtdc_db/management/commands/seed.py
+++ ngtdc_db/management/commands/seed.py
@@ -9,55 +9,54 @@ class Command(BaseCommand):
     help = "Seed the database"
     
     def add_arguments(self, parser):
-        """Add 'filepath' argument for manage.py seed command."""
+        """Add 'file' argument for manage.py seed command."""
 
         parser.add_argument(
             "-f",
-            "--filepath",
-            help = 'The path to the data. Should be .xlsx file format.',
+            "--file",
+            help = 'The test directory version being used.',
 			nargs = 1,
             )
 
 
-    def clean_data(self, filepath):
-        """Call get_data.py functions on the specified file.
+    def clean_data(self, file_version):
+        """Call get_data.py functions on the specified test directory version.
 
         Args:
-            filepath: path to .xlsx file containing test directory data
             version: test directory version (1 or 2)
         
         Returns:
-            single_df [pandas dataframe]: contains data used to populate Django
-            database
+            single_df [pandas dataframe]: data from test directory .xlsx file
         
         """
 
-        print(filepath)
-
-        version = ''
-        while (version != '1') and (version != '2'):
-            version = input('Please specify test directory version:'\
-                '\n(1) November 2020'\
-                '\n(2) July 2021'\
-                '\n')
-
+        xl_file = ''
         data = ''
 
-        if version == '1':
-            data = get_data_v1.Data(filepath)
+        if file_version == '1':
+            xl_file = """National-Genomic-Test-Directory-Cancer-November-2020-21.xlsx"""
 
-        elif version == '2':
-            data = get_data_v2.Data(filepath)
+            data = get_data_v1.Data(xl_file)
+            print('Seeding with version 1 file:', xl_file)
+        
+        elif file_version == '2':
+            xl_file = """CONFIDENTIAL-Final-National-Genomic-Test-Directory-Cancer-20-21-v2_pstb.xlsx"""
+
+            data = get_data_v2.Data(xl_file)
+            print('Seeding with version 2 file:', xl_file)
+        
+        else:
+            raise ValueError('Specified directory version must be 1 or 2.')
 
         # Functions which apply to a dictionary of pandas dataframes
-        df_dict = data.get_xl_data(filepath)
+        df_dict = data.get_xl_data(xl_file)
         data.remove_blank_rows(df_dict)
         data.rename_columns(df_dict)
         data.replace_merged_cells(df_dict)
         data.default_blank_values(df_dict)
         data.add_new_fields(df_dict)
 
-        if version == '2':
+        if file_version == '2':
             data.TEMPORARY_FIX_BLANK_TCS(df_dict)
 
         # Functions which apply to a single consolidated dataframe
@@ -65,23 +64,24 @@ class Command(BaseCommand):
         data.all_cells_to_strings(single_df)
         data.targets_to_lists(single_df)
 
-        return single_df, version
+        return single_df, file_version
     
 
     def handle(self, *args, **kwargs):
         """Gets data from .xlsx file and inserts into Django database."""
 
         # Requires a filepath to be provided
-        if kwargs['filepath']:
-            filepath = kwargs['filepath']
+        if kwargs['file']:
+            version = kwargs['file']
 
             # Applies functions in get_data.py script to provided .xlsx file
-            cleaned_df, version = self.clean_data(filepath[0])
+            cleaned_df, version = self.clean_data(version[0])
             print('Pandas dataframe created from Excel file.')
+            print('Populating Django database models...')
 
             # Inserts data into Django database
             inserter.insert_data(cleaned_df, version)
             print('Database population completed.')
         
         else:
-            print('Please supply path to file as -f or --filepath argument')
+            print('Directory version required in -v / --version argument.')
