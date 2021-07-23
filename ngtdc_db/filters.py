fbbcbbf3b3e7b9a45d1be9bba34f9c074a8eacb3
Jay-Miles <jay.miles@addenbrookes.nhs.uk>
2021-07-23 13:37:39 +0100

    indented some filter boxes

--- ngtdc_db/filters.py
+++ ngtdc_db/filters.py
@@ -18,7 +18,7 @@ class Jul21MainFilter(django_filters.FilterSet):
         label='CI Code',
         help_text = '(Clinical indication code e.g. M1)',
         lookup_expr='icontains',
-        widget=forms.TextInput(attrs={'style': 'max-width: 10vw; margin: 0 auto',})
+        widget=forms.TextInput(attrs={'style': 'max-width: 10vw; margin-left: 100px',})
         )
 
     ci_name = django_filters.CharFilter(
@@ -26,7 +26,7 @@ class Jul21MainFilter(django_filters.FilterSet):
         label='CI Name',
         help_text = '(Clinical indication name e.g. Colorectal carcinoma)',
         lookup_expr='icontains',
-        widget=forms.TextInput(attrs={'style': 'max-width: 10vw;',})
+        widget=forms.TextInput(attrs={'style': 'max-width: 10vw; margin-left: 94px',})
         )
 
     test_code = django_filters.CharFilter(
@@ -34,7 +34,7 @@ class Jul21MainFilter(django_filters.FilterSet):
         label='Test Code',
         help_text = '(Test code e.g. M1.2)',
         lookup_expr='icontains',
-        widget=forms.TextInput(attrs={'style': 'max-width: 10vw;',})
+        widget=forms.TextInput(attrs={'style': 'max-width: 10vw; margin-left: 82px',})
         )
 
     test_name = django_filters.CharFilter(
@@ -42,7 +42,7 @@ class Jul21MainFilter(django_filters.FilterSet):
         label='Test Name',
         help_text = '(Test name e.g. KRAS hotspot)',
         lookup_expr='icontains',
-        widget=forms.TextInput(attrs={'style': 'max-width: 10vw;',})
+        widget=forms.TextInput(attrs={'style': 'max-width: 10vw; margin-left: 77px',})
         )
 
     targets_essential = django_filters.CharFilter(
@@ -51,7 +51,7 @@ class Jul21MainFilter(django_filters.FilterSet):
         help_text = '(Test targets e.g. KRAS)',
         lookup_expr='icontains',
         distinct = True,
-        widget=forms.TextInput(attrs={'style': 'max-width: 10vw;',})
+        widget=forms.TextInput(attrs={'style': 'max-width: 10vw; margin-left: 15px',})
         )
 
     targets_desirable = django_filters.CharFilter(
@@ -60,25 +60,65 @@ class Jul21MainFilter(django_filters.FilterSet):
         help_text = '(Test targets e.g. KRAS)',
         lookup_expr='icontains',
         distinct = True,
-        widget=forms.TextInput(attrs={'style': 'max-width: 10vw;',})
+        widget=forms.TextInput(attrs={'style': 'max-width: 10vw; margin-left: 11px',})
         )
 
     tt_code = django_filters.CharFilter(
         field_name='tt_id__tt_code',
         label='TT Code',
         lookup_expr='icontains',
-        widget=forms.TextInput(attrs={'style': 'max-width: 10vw;',})
+        widget=forms.TextInput(attrs={'style': 'max-width: 10vw; margin-left: 96px',})
         )
+
+    # ci_code__cancer_id = django_filters.ChoiceFilter(
+    #     field_name='ci_code__cancer_id',
+    #     label='Cancer Type',
+    #     widget=forms.Select(attrs={'style': 'max-width: 10vw; margin-left: 100px',})
+    #     )
+    
+    # scope_id = django_filters.ChoiceFilter(
+    #     field_name='scope_id',
+    #     label='Test Scope',
+    #     widget=forms.Select(attrs={'style': 'max-width: 10vw; margin-left: 100px',})
+    #     )
+    
+    # tech_id = django_filters.ChoiceFilter(
+    #     field_name='tech_id',
+    #     label='Technology',
+    #     widget=forms.Select(attrs={'style': 'max-width: 10vw; margin-left: 100px',})
+    #     )
+    
+    # inhouse_id = django_filters.ChoiceFilter(
+    #     field_name='inhouse_id',
+    #     label='In-House Test',
+    #     widget=forms.Select(attrs={'style': 'max-width: 10vw; margin-left: 100px',})
+    #     )
+    
+    # provided_id = django_filters.ChoiceFilter(
+    #     field_name='provided_id',
+    #     label='Currently Provided',
+    #     widget=forms.Select(attrs={'style': 'max-width: 10vw; margin-left: 100px',})
+    #     )
 
     class Meta:
         model = GenomicTestJul21
+
         fields = [
             'ci_code__cancer_id',
             'scope_id',
             'tech_id',
             'inhouse_id',
             'provided_id',
-            ]
+        ]
+
+        exclude = [
+            'cc_id',
+            'specialist_id',
+            'eligibility',
+            'family_id',
+            'citt_id',
+            'tt_id'
+        ]
 
 
 class Jul21CIFilter(django_filters.FilterSet):
@@ -91,6 +131,7 @@ class Jul21CIFilter(django_filters.FilterSet):
         label='CI Code',
         help_text = '(Clinical indication code e.g. M1)',
         lookup_expr='icontains',
+        widget=forms.TextInput(attrs={'style': 'max-width: 10vw; margin-left: 38px',}),
         )
 
     ci_name = django_filters.CharFilter(
@@ -98,6 +139,7 @@ class Jul21CIFilter(django_filters.FilterSet):
         label='CI Name',
         help_text = '(Clinical indication name e.g. Colorectal carcinoma)',
         lookup_expr='icontains',
+        widget=forms.TextInput(attrs={'style': 'max-width: 10vw; margin-left: 32px',})
         )
 
     class Meta:
@@ -118,6 +160,7 @@ class Nov20MainFilter(django_filters.FilterSet):
         label='CI Code',
         help_text = '(Clinical indication code e.g. M1)',
         lookup_expr='icontains',
+        widget=forms.TextInput(attrs={'style': 'max-width: 10vw; margin-left: 100px',})
         )
 
     ci_name = django_filters.CharFilter(
@@ -125,6 +168,7 @@ class Nov20MainFilter(django_filters.FilterSet):
         label='CI Name',
         help_text = '(Clinical indication name e.g. Colorectal carcinoma)',
         lookup_expr='icontains',
+        widget=forms.TextInput(attrs={'style': 'max-width: 10vw; margin-left: 94px',})
         )
 
     test_code = django_filters.CharFilter(
@@ -132,6 +176,7 @@ class Nov20MainFilter(django_filters.FilterSet):
         label='Test Code',
         help_text = '(Test code e.g. M1.2)',
         lookup_expr='icontains',
+        widget=forms.TextInput(attrs={'style': 'max-width: 10vw; margin-left: 82px',})
         )
 
     test_name = django_filters.CharFilter(
@@ -139,25 +184,62 @@ class Nov20MainFilter(django_filters.FilterSet):
         label='Test Name',
         help_text = '(Test name e.g. KRAS hotspot)',
         lookup_expr='icontains',
+        widget=forms.TextInput(attrs={'style': 'max-width: 10vw; margin-left: 77px',})
         )
 
     targets_essential = django_filters.CharFilter(
         field_name='targets_essential__target',
-        label='Targets',
+        label='Targets (Essential)',
         help_text = '(Test targets e.g. KRAS)',
         lookup_expr='icontains',
         distinct = True,
+        widget=forms.TextInput(attrs={'style': 'max-width: 10vw; margin-left: 15px',})
         )
+
+    # ci_code__cancer_id = django_filters.ChoiceFilter(
+    #     field_name='ci_code__cancer_id',
+    #     label='Cancer Type',
+    #     widget=forms.Select(attrs={'style': 'max-width: 10vw; margin-left: 100px',})
+    #     )
+    
+    # scope_id = django_filters.ChoiceFilter(
+    #     field_name='scope_id',
+    #     label='Test Scope',
+    #     widget=forms.Select(attrs={'style': 'max-width: 10vw; margin-left: 100px',})
+    #     )
+    
+    # tech_id = django_filters.ChoiceFilter(
+    #     field_name='tech_id',
+    #     label='Technology',
+    #     widget=forms.Select(attrs={'style': 'max-width: 10vw; margin-left: 100px',})
+    #     )
+    
+    # inhouse_id = django_filters.ChoiceFilter(
+    #     field_name='inhouse_id',
+    #     label='In-House Test',
+    #     widget=forms.Select(attrs={'style': 'max-width: 10vw; margin-left: 100px',})
+    #     )
+    
+    # provided_id = django_filters.ChoiceFilter(
+    #     field_name='provided_id',
+    #     label='Currently Provided',
+    #     widget=forms.Select(attrs={'style': 'max-width: 10vw; margin-left: 100px',})
+    #     )
 
     class Meta:
         model = GenomicTestNov20
+
         fields = [
             'ci_code__cancer_id',
             'scope_id',
             'tech_id',
             'inhouse_id',
             'provided_id',
-            ]
+        ]
+
+        exclude = [
+            'eligibility',
+        ]
 
 
 class Nov20CIFilter(django_filters.FilterSet):
@@ -170,6 +252,7 @@ class Nov20CIFilter(django_filters.FilterSet):
         label='CI Code',
         help_text = '(Clinical indication code e.g. M1)',
         lookup_expr='icontains',
+        widget=forms.TextInput(attrs={'style': 'max-width: 10vw; margin-left: 38px',}),
         )
 
     ci_name = django_filters.CharFilter(
@@ -177,6 +260,7 @@ class Nov20CIFilter(django_filters.FilterSet):
         label='CI Name',
         help_text = '(Clinical indication name e.g. Colorectal carcinoma)',
         lookup_expr='icontains',
+        widget=forms.TextInput(attrs={'style': 'max-width: 10vw; margin-left: 32px',})
         )
 
     class Meta:
