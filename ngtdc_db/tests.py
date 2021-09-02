117eb27ce84550e665895637223d4f87103d385b
Jay-Miles <jay.miles@addenbrookes.nhs.uk>
2021-09-02 14:41:32 +0100

    made some templates slightly neater

--- ngtdc_db/tests.py
+++ ngtdc_db/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
 
-# Create your tests here.
+# tests which need to access the db should use django.test.TestCase
+# others can use unittest.TestCase
+# run tests with python manage.py test
+# you can run tests in reverse to confirm that they're independent-
+# python manage.py test --reverse
